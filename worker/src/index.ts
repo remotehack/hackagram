771c86edf26c7cebc5a009b804a96d5ed0ce192e
Ben Foxall <benfoxall@gmail.com>
2023-10-29 23:13:39 +0000

    Just call the userinfo api!

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,5 +1,4 @@
 console.log('Hello from worker!');
-import * as jose from 'jose';
 
 const corsHeaders = {
 	'Access-Control-Allow-Origin': '*',
@@ -8,36 +7,26 @@ const corsHeaders = {
 	Allow: 'GET, HEAD, POST, OPTIONS',
 };
 
-const config = {
-	audience: 'https://hackergram.benfoxall.workers.dev',
-	issuerBaseURL: 'https://dev-l2qhix4unqfdzpmz.uk.auth0.com/',
-	tokenSigningAlg: 'RS256',
-};
-
-const JWKS = jose.createRemoteJWKSet(new URL('https://dev-l2qhix4unqfdzpmz.uk.auth0.com/.well-known/jwks.json'));
-
 export default {
 	async fetch(request, env, ctx) {
-		console.log(jose);
+		let name = 'Hackers!';
 
 		const auth = request.headers.get('Authorization');
 		if (auth) {
-			const jwt = auth.split(' ')[1];
-
-			console.log('JTW', jwt);
-
-			const { payload, protectedHeader } = await jose.jwtVerify(jwt, JWKS, {
-				issuer: config.issuerBaseURL,
-				audience: config.audience,
-				algorithms: [config.tokenSigningAlg],
+			const response = await fetch('https://dev-l2qhix4unqfdzpmz.uk.auth0.com/userinfo', {
+				method: 'GET',
+				headers: {
+					Authorization: auth,
+				},
 			});
-			console.log(protectedHeader);
-			console.log(payload);
+			const user = await response.json();
+			console.log(user);
+			name = user.name;
 		} else {
 			console.log('no auth');
 		}
 
-		return new Response('Hello Hackers1!!', {
+		return new Response(`Hello ${name}!!`, {
 			headers: {
 				...corsHeaders,
 				'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers'),
